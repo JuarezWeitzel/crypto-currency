867ea844deb42b6db271a75cbb7148025c7c0ecc
JuarezWeitzel <juarez.zbw@gmail.com>
2024-03-12 20:20:38 -0300

    feat: Create and temporarily style Header component
    This commit adds the Header component to the project and applies temporary styling to it. The Header is now visible and styled to provide basic functionality, serving as a placeholder until further styling adjustments are made.

--- Cryptocurrency Project/src/components/header/index.tsx
+++ Cryptocurrency Project/src/components/header/index.tsx
@@ -1,9 +1,13 @@
-
+import { Link } from 'react-router-dom'
+import { HeaderContainer  } from './style'
+import logoimg from '../../assets/logo.svg'
 
 export const Header = () => {
     return(
-        <div>
-            <h1>Header</h1>
-        </div>
+        <HeaderContainer>
+                <Link to="/">
+                    <img src={logoimg} alt="Logo Crypto"/>
+                </Link>
+        </HeaderContainer>
     )
 }
