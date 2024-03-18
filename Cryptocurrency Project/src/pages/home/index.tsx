27ca0869c30f04a65d81d0b423fd68ed4698808d
JuarezWeitzel <juarez.zbw@gmail.com>
2024-03-18 14:44:22 -0300

    feat: Add TableCurrency component to Home page
    This commit adds the TableCurrency component to the Home page. Now, the Home page displays the currency table, allowing users to view currency information directly from the main page.

--- Cryptocurrency Project/src/pages/home/index.tsx
+++ Cryptocurrency Project/src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import { Search } from "../../components/search"
+import { TableCurrency } from "../../components/tableCurrency"
 
 
 export const Home = () => {
     return(
         <div>
             <Search/>
+            <TableCurrency/>
         </div>
     )
 }
