7518b8aa08498ec7fe3a577bd5225469e6e62885
JuarezWeitzel <juarez.zbw@gmail.com>
2024-03-21 19:27:55 -0300

    feat: Implement search functionality
    This commit adds the search functionality to the application. The search function has been implemented and is now working as expected, allowing users to search for specific currency information.

--- Cryptocurrency Project/src/components/search/index.tsx
+++ Cryptocurrency Project/src/components/search/index.tsx
@@ -1,11 +1,27 @@
+import { FormEvent, useState } from "react"
 import { MainContainer, SearchForm, SearchInput, SearchButton } from "./style"
 import { BiSearch } from "react-icons/bi"
+import { useNavigate } from "react-router-dom";
 
 export const Search = () => {
+
+    const [inputValue, setInputValue] = useState("");
+    const navigate = useNavigate();
+
+    const handleSearch = (event: FormEvent) => {
+        event.preventDefault();
+
+        if(inputValue === "") return;
+
+        navigate(`/detail/${inputValue}`)
+    }
+
     return(
         <MainContainer>
-            <SearchForm action="">
+            <SearchForm onSubmit={handleSearch}>
                 <SearchInput 
+                value={inputValue}
+                onChange={(event) => setInputValue(event.target.value)}
                 placeholder="Enter currency symbol: Example BTC"
                 type="text"/>
                 <SearchButton
