3b635d5df70f5785c7a22c080474b389fd4c1271
JuarezWeitzel <juarez.zbw@gmail.com>
2024-03-25 15:57:12 -0300

    Refactor: Replaced <div></div> with <></> for cleaner code

--- Cryptocurrency Project/src/pages/home/index.tsx
+++ Cryptocurrency Project/src/pages/home/index.tsx
@@ -4,9 +4,9 @@ import { TableCurrency } from "../../components/tableCurrency"
 
 export const Home = () => {
     return(
-        <div>
+        <>
             <Search/>
             <TableCurrency/>
-        </div>
+        </>
     )
 }
