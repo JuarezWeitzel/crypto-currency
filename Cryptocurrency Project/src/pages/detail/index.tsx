ed382e0f4f8b965ff8973f3164f3bcb98237d00b
JuarezWeitzel <juarez.zbw@gmail.com>
2024-03-25 16:19:04 -0300

    feat: Refactor Detail component to use React hooks and update data formatting
    - Updated component to utilize React hooks (useEffect, useState) for state management.
    - Utilized useParams and useNavigate hooks from react-router-dom for accessing route parameters and navigation.
    - Defined interface CoinProp to specify the shape of coin data.
    - Modified useEffect to fetch data from the API and update state accordingly.
    - Implemented data formatting using Intl.NumberFormat to display prices in USD currency.
    - Added loading state and conditional rendering to display a loading message while data is being fetched.

--- Cryptocurrency Project/src/pages/detail/index.tsx
+++ Cryptocurrency Project/src/pages/detail/index.tsx
@@ -1,7 +1,66 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom"
 
-
+interface CoinProp{
+    symbol: string,
+    name: string,
+    price: string,
+    market_cap: string,
+    low_24h: string,
+    high_24h: string,
+    total_volume_24h: string,
+    delta_24h: string,
+    formatedPrice: string,
+    formatedMarket: string,
+    formatedLowPrice: string,
+    formatedHighPrice: string,
+    numberDelta?: number,
+}
 
 export const Detail = () => {
+    const {cripto} = useParams();
+    const [detail, setDetail] = useState<CoinProp>()
+    const [loading, setLoading] = useState(true)
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+        const getData = () => {
+            fetch(`https://sujeitoprogramador.com/api-cripto/coin/?key=3f7b6e5897e7211f&symbol=${cripto}`)
+            .then((response) => response.json())
+            .then((data) => {
+                
+                let price = Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD"
+                })
+
+                const resultData = {
+                    ...data,
+                    formatedPrice: price.format(Number(data.price)),
+                    formatedMarketCap: price.format(Number(data.market_cap)),
+                    formatedLowPrice: price.format(Number(data.low_24h)),
+                    formatedHighPrice: price.format(Number(data.high_24h)),
+                }
+                
+                setDetail(resultData);
+                setLoading(false);
+            }).catch(error => {
+                console.log(error)
+                navigate("*")
+            })
+        }
+
+        getData();
+
+    },[cripto])
+
+    if(loading) {
+        <div>
+            <h4>Carregando Informações...</h4>
+        </div>
+    }
+
     return(
         <div>
             Página Detail
