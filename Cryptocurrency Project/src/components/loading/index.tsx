f671a6aa6b0a7bcf46c7984e0a8eecb07042715e
JuarezWeitzel <juarez.zbw@gmail.com>
2024-03-27 17:25:56 -0300

    feat: Implement loading logic using LoadingContext in Loading component
    This commit adds logic to the Loading component to utilize the LoadingContext API. Now, the Loading component will interact with the LoadingContext to manage loading states across the application, providing a centralized mechanism for handling loading indicators.

--- Cryptocurrency Project/src/components/loading/index.tsx
+++ Cryptocurrency Project/src/components/loading/index.tsx
@@ -1,12 +1,20 @@
+import { useLoading } from '../../context/loading/loadingContext';
 import * as S from './style';
 
 export const Loading = () => {
+    const { loading } = useLoading();
+
     return(
+        (loading && loading === true ? (
         <S.Container>
             <S.LoaderContainer>
                 <S.Loader></S.Loader>
                 <S.LoaderText>Loading...</S.LoaderText>
             </S.LoaderContainer>
         </S.Container>
+        ) : (
+            <>
+            </>
+        ))
     )
 }
