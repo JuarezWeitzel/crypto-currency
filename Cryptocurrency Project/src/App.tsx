1331ef4cb6072189c3fe329a16fae46f6549034a
JuarezWeitzel <juarez.zbw@gmail.com>
2024-03-27 17:47:06 -0300

    feat: Pass LoadingProvider to routes and layout components
    This commit updates the routes and layout components to include the LoadingProvider, ensuring that loading state management is available throughout the application. Now, all components rendered within the routes and layout will have access to the LoadingContext API for handling loading indicators.

--- Cryptocurrency Project/src/App.tsx
+++ Cryptocurrency Project/src/App.tsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
+import { LoadingProvider } from "./context/loading/loadingContext"
 
 function App() {
 
   return (
-      <RouterProvider router={router}/>
+      <LoadingProvider>
+        <RouterProvider router={router}/>
+      </LoadingProvider>
     )
 }
 
--- Cryptocurrency Project/src/components/layout/index.tsx
+++ Cryptocurrency Project/src/components/layout/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "../header"
+import { Loading } from "../loading"
 
 export const Layout = () => {
     return(
         <>
             <Header/>
             <Outlet/>
+            <Loading/>
         </>
     )
 }
