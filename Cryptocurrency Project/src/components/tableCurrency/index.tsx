91a954fcae269c5bbdef29bfc805b7eb1b2d3bd7
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-02 15:14:09 -0300

    feat: Implement pagination system for viewing all cryptocurrencies
    This commit introduces a pagination system to the project, enabling users to navigate through multiple pages to view all available cryptocurrencies.

--- Cryptocurrency Project/src/components/tableCurrency/index.tsx
+++ Cryptocurrency Project/src/components/tableCurrency/index.tsx
@@ -21,6 +21,7 @@ interface DataProps {
 
 export const TableCurrency = () => {
   const [coins, setCoins] = useState<CoinProps[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const [error, setError] = useState<string | null>(null)
   const { setLoading } = useLoading();
 
@@ -30,7 +31,7 @@ export const TableCurrency = () => {
       fetch("https://sujeitoprogramador.com/api-cripto/?key=3f7b6e5897e7211f")
         .then((response) => response.json())
         .then((data: DataProps) => {
-          let coinsData = data.coins.slice(0, 10);
+          let coinsData = data.coins.slice(0, 20);
 
           let price = Intl.NumberFormat("en-US", {
             style: "currency",
@@ -64,40 +65,77 @@ export const TableCurrency = () => {
     return <S.CatchError> Sorry! Request limit exhausted, try in an hour! </S.CatchError>
   }
 
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(coins.length / itemsPerPage);
+
+  const goToPage = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <S.ResponsiveTable>
-      <thead>
-        <S.TableRow>
-          <S.TableHeader>Crypto</S.TableHeader>
-          <S.TableHeader>Market Value</S.TableHeader>
-          <S.TableHeader>Price</S.TableHeader>
-          <S.TableHeader>Volume</S.TableHeader>
-        </S.TableRow>
-      </thead>
-      <tbody>
-        {coins.map((coin) => (
-          <S.TableRow key={coin.name}>
-            <S.LabelTableCell data-label="Crypto">
-              <S.Link to={`/detail/${coin.symbol}`}>
-                {coin.name} | {coin.symbol}
-              </S.Link>
-            </S.LabelTableCell>
-            <S.LabelTableCell data-label="Market Value">
-              {coin.formatedMarket}
-            </S.LabelTableCell>
-            <S.LabelTableCell data-label="Price">
-              {coin.formatedPrice}
-            </S.LabelTableCell>
-            <S.LabelTableCell
-              data-label="Volume"
-              profit={coin.numberDelta ? coin.numberDelta >= 0 : false}
-              loss={coin.numberDelta ? coin.numberDelta < 0 : false}
-            >
-              {coin.delta_24h}
-            </S.LabelTableCell>
+    <>
+      <S.ResponsiveTable>
+        <thead>
+          <S.TableRow>
+            <S.TableHeader>Crypto</S.TableHeader>
+            <S.TableHeader>Market Value</S.TableHeader>
+            <S.TableHeader>Price</S.TableHeader>
+            <S.TableHeader>Volume</S.TableHeader>
           </S.TableRow>
+        </thead>
+        <tbody>
+          {coins.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+            .map((coin) => (
+              <S.TableRow key={coin.name}>
+                <S.LabelTableCell data-label="Crypto">
+                  <S.Link to={`/detail/${coin.symbol}`}>
+                    {coin.name} | {coin.symbol}
+                  </S.Link>
+                </S.LabelTableCell>
+                <S.LabelTableCell data-label="Market Value">
+                  {coin.formatedMarket}
+                </S.LabelTableCell>
+                <S.LabelTableCell data-label="Price">
+                  {coin.formatedPrice}
+                </S.LabelTableCell>
+                <S.LabelTableCell
+                  data-label="Volume"
+                  profit={coin.numberDelta ? coin.numberDelta >= 0 : false}
+                  loss={coin.numberDelta ? coin.numberDelta < 0 : false}
+                >
+                  {coin.delta_24h}
+                </S.LabelTableCell>
+              </S.TableRow>
+            ))}
+        </tbody>
+      </S.ResponsiveTable>
+      <S.Pages>
+        <S.ButtonPage
+          onClick={() => goToPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          &larr;
+        </S.ButtonPage>
+        {Array.from(Array(totalPages).keys()).map((page) => (
+          <S.ButtonPage
+            key={page}
+            onClick={() => goToPage(page + 1)}
+            disabled={currentPage === page + 1}
+            style={{
+              color: currentPage === page + 1 ? "#FFF" : "",
+              backgroundColor: currentPage === page + 1 ? "#30beff" : ""
+            }}
+          >
+            {page + 1}
+          </S.ButtonPage>
         ))}
-      </tbody>
-    </S.ResponsiveTable>
+        <S.ButtonPage
+          onClick={() => goToPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          &rarr;
+        </S.ButtonPage>
+      </S.Pages>
+    </>
   );
 };
