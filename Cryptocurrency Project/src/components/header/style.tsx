ce5e196f3690079d180d5bde66442828af701176
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-02 15:02:28 -0300

    style: Improve responsiveness of header logo
    This commit enhances the responsiveness of the header logo to ensure it displays optimally across various screen sizes and devices. By making adjustments to the logo's styling, it maintains its appearance and readability on both desktop and mobile devices.

--- Cryptocurrency Project/src/components/header/style.tsx
+++ Cryptocurrency Project/src/components/header/style.tsx
@@ -1,9 +1,21 @@
+import { Link as StyledLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 120px;
-    max-width: 1080px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 120px;
+  max-width: 1080px;
+  margin: 0 auto;
+
+  @media screen and (max-width: 350px) {
+    img {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 80%;
+      margin: 0 auto;
+    }
+  }
 `;
