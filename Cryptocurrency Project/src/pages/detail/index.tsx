2dce06373e7ce58d719a2e131b17aac273a05708
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-02 15:05:23 -0300

    feat: Create Detail section and style it
    This commit adds the Detail section to the project and applies styling to ensure its visual coherence and aesthetic appeal. The Detail section provides detailed information about a specific item and is now visually consistent with the overall design of the project.

--- Cryptocurrency Project/src/pages/detail/index.tsx
+++ Cryptocurrency Project/src/pages/detail/index.tsx
@@ -1,67 +1,104 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 import { useLoading } from "../../context/loading/loadingContext";
-import { Loading } from "../../components/loading";
+import * as S from "./style";
 
-interface CoinProp{
-    symbol: string,
-    name: string,
-    price: string,
-    market_cap: string,
-    low_24h: string,
-    high_24h: string,
-    total_volume_24h: string,
-    delta_24h: string,
-    formatedPrice: string,
-    formatedMarket: string,
-    formatedLowPrice: string,
-    formatedHighPrice: string,
-    numberDelta?: number,
+interface CoinProp {
+  symbol: string;
+  name: string;
+  price: string;
+  market_cap: string;
+  low_24h: string;
+  high_24h: string;
+  total_volume_24h: string;
+  delta_24h: string;
+  formatedPrice: string;
+  formatedMarket: string;
+  formatedLowPrice: string;
+  formatedHighPrice: string;
+  formatedMarketCap: string;
+  numberDelta?: number;
 }
 
 export const Detail = () => {
-    const {cripto} = useParams();
-    const [detail, setDetail] = useState<CoinProp>()
-    const { setLoading } = useLoading();
-    const navigate = useNavigate();
+  const { cripto } = useParams();
+  const [detail, setDetail] = useState<CoinProp>();
+  const { setLoading } = useLoading();
+  const navigate = useNavigate();
 
-    useEffect(() => {
+  useEffect(() => {
+    const getData = () => {
+      setLoading(true);
+      fetch(
+        `https://sujeitoprogramador.com/api-cripto/coin/?key=3f7b6e5897e7211f&symbol=${cripto}`
+      )
+        .then((response) => response.json())
+        .then((data: CoinProp) => {
+          let price = Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          });
 
-        const getData = () => {
-            setLoading(true)
-            fetch(`https://sujeitoprogramador.com/api-cripto/coin/?key=3f7b6e5897e7211f&symbol=${cripto}`)
-            .then((response) => response.json())
-            .then((data) => {
-                
-                let price = Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "USD"
-                })
+          const resultData = {
+            ...data,
+            formatedPrice: price.format(Number(data.price)),
+            formatedMarketCap: price.format(Number(data.market_cap)),
+            formatedLowPrice: price.format(Number(data.low_24h)),
+            formatedHighPrice: price.format(Number(data.high_24h)),
+            numberDelta: parseFloat(data.delta_24h.replace(",", ".")),
+          };
 
-                const resultData = {
-                    ...data,
-                    formatedPrice: price.format(Number(data.price)),
-                    formatedMarketCap: price.format(Number(data.market_cap)),
-                    formatedLowPrice: price.format(Number(data.low_24h)),
-                    formatedHighPrice: price.format(Number(data.high_24h)),
-                }
-                
-                setDetail(resultData);
-            }).catch(error => {
-                console.log(error)
-                navigate("*")
-            }).finally(() => {
-                setLoading(false);
-            })
-        }
+          setDetail(resultData);
+        })
+        .catch((error) => {
+          console.log(error);
+          navigate("*");
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
 
-        getData();
+    getData();
+  }, [cripto]);
 
-    },[cripto])
+  return (
+    <S.Container>
+      <S.DetailDiv>
+        <S.DetailName> {detail?.name} </S.DetailName>
+        <S.DetailSymbol> {detail?.symbol} </S.DetailSymbol>
 
-    return(
-        <div>
-            Página Detail
-        </div>
-    )
-}
+        <S.DetailSection>
+          <p>
+            <strong>Market Cap: </strong>
+            <span> {detail?.formatedMarketCap} </span>
+          </p>
+          <p>
+            <strong>Price: </strong> <span> {detail?.formatedPrice} </span>
+          </p>
+          <p>
+            <strong>High Price: </strong>
+            <span> {detail?.formatedHighPrice} </span>
+          </p>
+          <p>
+            <strong>Low Price: </strong>
+            <span> {detail?.formatedLowPrice} </span>
+          </p>
+          <p>
+            <strong>Volume: </strong> 
+            <S.Color
+            profit={detail?.numberDelta ? detail.numberDelta >= 0 : false}
+            loss={detail?.numberDelta ? detail.numberDelta < 0 : false}
+            > 
+            {detail?.delta_24h}
+            </S.Color>
+          </p>
+        </S.DetailSection>
+      </S.DetailDiv>
+
+      <S.Link to="/">
+        List Coins
+      </S.Link>
+    </S.Container>
+  );
+};
--- Cryptocurrency Project/src/pages/detail/style.tsx
+++ Cryptocurrency Project/src/pages/detail/style.tsx
@@ -1,1 +1,90 @@
-import styled from "styled-components";
+import { Link as StyledLink } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+
+interface StyledProps {
+    profit?: boolean;
+    loss?: boolean;
+  }
+
+export const Container = styled.div`
+    text-align: center;
+`;
+
+export const DetailDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 90%;
+    margin: 10px auto 30px;
+    color: #FFF;
+    background-color: #444444;
+    padding: 10px;
+    border-radius: 8px;
+
+    p {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+    }
+
+    span, p {
+        font-size: 20px;
+
+        @media screen and (max-width: 450px) {
+        font-size: 15px;
+    }
+
+        @media screen and (max-width: 330px) {
+        font-size: 12px;
+    }
+    }
+`;
+
+export const DetailName = styled.h1`
+    color: #30beff;
+    margin: 0px auto;
+`;
+
+export const DetailSymbol = styled.h2`
+    margin: 10px auto 10px;
+`;
+
+export const DetailSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    background-color: #30beff;
+    border-radius: 8px;
+`;
+
+export const Color = styled.span<StyledProps>`
+    font-weight: bolder;
+
+    ${props =>
+    props.profit &&
+    css`
+      color: #12f98a;
+    `}
+
+  ${props =>
+    props.loss &&
+    css`
+      color: #f91257;
+    `}
+`;
+
+export const Link = styled(StyledLink)`
+    padding: 10px 40px;
+    color: #FFF;
+    text-decoration: none;
+    font-size: 20px;
+    background-color: #30beff;
+    border-radius: 8px;
+    transition: 0.4s;
+
+    &:hover {
+        background-color: #444444;
+        color: #30beff;
+    }
+`;
