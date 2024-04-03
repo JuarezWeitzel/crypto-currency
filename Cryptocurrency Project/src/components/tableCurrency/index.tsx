0458c797ef82cfdbcb24f3649ca5eaa0da154cdd
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-03 16:06:16 -0300

    feat: Implement localStorage for handling request limit
    This commit introduces localStorage to the project to handle the case when the request limit is reached. When the request limit is exceeded, the application will use data stored in localStorage to render the list of coins until the request limit returns to normal. This ensures uninterrupted access to coin data for users even during periods of high traffic or request limits.

--- Cryptocurrency Project/src/components/tableCurrency/index.tsx
+++ Cryptocurrency Project/src/components/tableCurrency/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 import * as S from "./style";
 import { useLoading } from "../../context/loading/loadingContext";
 
@@ -21,59 +20,74 @@ interface DataProps {
 
 export const TableCurrency = () => {
   const [coins, setCoins] = useState<CoinProps[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [error, setError] = useState<string | null>(null)
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [error, setError] = useState<string | null>(null);
   const { setLoading } = useLoading();
 
+  const getData = () => {
+    setLoading(true);
+    fetch("https://sujeitoprogramador.com/api-cripto/?key=3f7b6e5897e7211f")
+      .then((response) => response.json())
+      .then((data: DataProps) => {
+        let coinsData = data.coins.slice(0, 30);
+
+        let price = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        });
+
+        const formatResult = coinsData.map((item: any) => {
+          const formated = {
+            ...item,
+            formatedPrice: price.format(Number(item.price)),
+            formatedMarket: price.format(Number(item.market_cap)),
+            numberDelta: parseFloat(item.delta_24h.replace(",", ".")),
+          };
+
+          return formated;
+        });
+
+        setCoins(formatResult);
+        localStorage.setItem("cachedCoins", JSON.stringify(formatResult));
+      })
+      .catch((error) => {
+        setError(error.message);
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   useEffect(() => {
-    function getData() {
-      setLoading(true);
-      fetch("https://sujeitoprogramador.com/api-cripto/?key=3f7b6e5897e7211f")
-        .then((response) => response.json())
-        .then((data: DataProps) => {
-          let coinsData = data.coins.slice(0, 20);
-
-          let price = Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-          });
-
-          const formatResult = coinsData.map((item) => {
-            const formated = {
-              ...item,
-              formatedPrice: price.format(Number(item.price)),
-              formatedMarket: price.format(Number(item.market_cap)),
-              numberDelta: parseFloat(item.delta_24h.replace(",", ".")),
-            };
-
-            return formated;
-          });
-
-          setCoins(formatResult);
-        })
-        .catch((error) => {
-          setError(error.message);
-        }).finally(() => {
-          setLoading(false);
-        })
+    const cachedData = localStorage.getItem("cachedCoins");
+    if (cachedData) {
+      setCoins(JSON.parse(cachedData));
     }
 
     getData();
   }, []);
 
-  if(error) {
-    return <S.CatchError> Sorry! Request limit exhausted, try in an hour! </S.CatchError>
-  }
+  useEffect(() => {
+    if (error === null) {
+      getData();
+    }
+  }, [error]);
 
-  const itemsPerPage = 10;
-  const totalPages = Math.ceil(coins.length / itemsPerPage);
+  const itemsPerPage: number = 10;
+  const totalPages: number = Math.ceil(coins.length / itemsPerPage);
 
-  const goToPage = (page: number) => {
+  const goToPage = (page: number): void => {
     setCurrentPage(page);
   };
 
   return (
     <>
+      {error && (
+        <S.CatchError>
+          Sorry! Request limit exhausted, try in an hour! In the meantime, the list will not be updated. <br/> The maximum number of requests per hour is 60.
+        </S.CatchError>
+      )}
       <S.ResponsiveTable>
         <thead>
           <S.TableRow>
@@ -116,14 +130,14 @@ export const TableCurrency = () => {
         >
           &larr;
         </S.ButtonPage>
-        {Array.from(Array(totalPages).keys()).map((page) => (
+        {Array.from(Array(totalPages).keys()).map((page, index) => (
           <S.ButtonPage
-            key={page}
+            key={index}
             onClick={() => goToPage(page + 1)}
             disabled={currentPage === page + 1}
             style={{
               color: currentPage === page + 1 ? "#FFF" : "",
-              backgroundColor: currentPage === page + 1 ? "#30beff" : ""
+              backgroundColor: currentPage === page + 1 ? "#30beff" : "",
             }}
           >
             {page + 1}
