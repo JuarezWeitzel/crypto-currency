69f006edd0b6ead8a9839747812812e74a55c8f8
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-10 17:47:10 -0300

    refactor: Remove Header from Layout

--- cryptocurrency-project/src/components/layout/index.tsx
+++ cryptocurrency-project/src/components/layout/index.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom"
-import { Header } from "../header"
 import { Loading } from "../loading"
 
 export const Layout = () => {
     return(
         <>
-            <Header/>
             <Loading/>
             <Outlet/>
         </>
