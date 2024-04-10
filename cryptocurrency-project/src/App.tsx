0fbb94cca162abfcbafb05170235e030cb22aee4
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-10 17:45:45 -0300

    feat: Insert Header, GlobalStyle, and Extend ThemeProvider Across App
    This commit integrates the `Header` component and the `GlobalStyle` into the `App` component, and extends the `ThemeProvider` to encompass the entire application. This enhancement ensures a cohesive styling experience and provides theme consistency throughout the app.

--- cryptocurrency-project/src/App.tsx
+++ cryptocurrency-project/src/App.tsx
@@ -1,13 +1,22 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
 import { LoadingProvider } from "./context/loading/loadingContext"
+import { ThemeProvider } from "styled-components"
+import dark from "./styles/themes/dark"
+import { Header } from "./components/header"
+import GlobalStyle from './styles/global.ts';
+import light from "./styles/themes/light"
 
 function App() {
 
   return (
+    <ThemeProvider theme={dark}>
       <LoadingProvider>
+        <GlobalStyle/>
+        <Header/>
         <RouterProvider router={router}/>
       </LoadingProvider>
+    </ThemeProvider>
     )
 }
 
