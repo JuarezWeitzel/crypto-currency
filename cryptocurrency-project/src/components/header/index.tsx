863bf9c95221ba7bf96946e5c2c46b14248c1055
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-15 13:46:46 -0300

    feat: Implement theme switch button in header
    This commit adds a theme switch button to the header, allowing users to toggle between different themes. The theme switch functionality enhances user customization options and provides a convenient way to change the visual appearance of the application.

--- cryptocurrency-project/src/components/header/index.tsx
+++ cryptocurrency-project/src/components/header/index.tsx
@@ -1,12 +1,39 @@
-import logoimg from '../../assets/logo.svg'
-import * as S from './style'
+import { HeaderContainer } from "./style";
+import logoimg from "../../assets/logo.svg";
+import React, { useContext } from "react";
+import { ThemeContext } from "styled-components";
+import Switch from 'react-switch';
+import * as S from './style';
 
-export const Header = () => {
-    return(
-        <S.HeaderContainer>
-                <S.Link href="/">
-                    <img src={logoimg} alt="Logo Crypto"/>
-                </S.Link>
-        </S.HeaderContainer>
-    )
+interface Props {
+  toggleTheme(): void;
 }
+
+export const Header: React.FC<Props> = ({ toggleTheme }) => {
+  const themeContext = useContext(ThemeContext);
+  const title = themeContext?.title || "light";
+
+  return (
+    <HeaderContainer>
+
+      <S.Link href="/">
+        <img src={logoimg} alt="Logo Crypto" />
+      </S.Link>
+
+
+      <Switch
+            onChange={toggleTheme}
+            checked={title === "light"}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            onColor="#30beff"
+            offColor="#444444"
+            offHandleColor="#30beff"
+            onHandleColor="#FFF"
+            handleDiameter={15}
+            width={40}
+            height={20}
+          />
+    </HeaderContainer>
+  );
+};
