112a89b5f1549f2affeb12917eb748e165675488
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-15 14:19:03 -0300

    style: Enhance responsiveness and adjust font size in Search component
    This commit improves the responsiveness of the Search component and adjusts the font size for better readability. These changes ensure that the Search component adapts well to various screen sizes and provides an optimal user experience.

--- cryptocurrency-project/src/components/search/style.tsx
+++ cryptocurrency-project/src/components/search/style.tsx
@@ -1,25 +1,45 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.main`
-    margin: 0 14px;
+  margin: 0 14px;
 `;
 
 export const SearchForm = styled.form`
-    display: flex;
-    gap: 14px;
-    width: 100%;
+  display: flex;
+  gap: 14px;
+  width: 100%;
 `;
 
 export const SearchInput = styled.input`
-    width: 100%;
-    height: 44px;
-    padding: 0 8px;
-    outline: none;
-    border: none;
-    border-radius: 8px;
+  width: 100%;
+  height: 44px;
+  padding: 0 8px;
+  font-size: 15px;
+  outline: none;
+  border: none;
+  border-radius: 8px;
+
+  &::-webkit-input-placeholder {
+    color: #000000;
+    font: 15px verdana, arial, sans-serif;
+  }
+
+  @media screen and (max-width: 386px) {
+    &::-webkit-input-placeholder {
+      color: #000000;
+      font: 12px verdana, arial, sans-serif;
+    }
+  }
+
+  @media screen and (max-width: 324px) {
+    &::-webkit-input-placeholder {
+      color: #000000;
+      font: 8.5px verdana, arial, sans-serif;
+    }
+  }
 `;
 
 export const SearchButton = styled.button`
-    border: 0;
-    background: transparent;
+  border: 0;
+  background: transparent;
 `;
