f80f2cfe1dae11cd00880fa39de4f7db105ff101
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-24 18:54:29 -0300

    feat: Adding icons for dark and light themes
    This commit adds icons to represent the dark and light themes in the project. The icons will be used to visually indicate the option to switch between the available themes, providing a more intuitive experience for users.

--- cryptocurrency-project/src/components/header/index.tsx
+++ cryptocurrency-project/src/components/header/index.tsx
@@ -2,8 +2,10 @@ import { HeaderContainer } from "./style";
 import logoimg from "../../assets/logo.svg";
 import React, { useContext } from "react";
 import { ThemeContext } from "styled-components";
-import Switch from 'react-switch';
-import * as S from './style';
+import Switch from "react-switch";
+import { CiSun } from "react-icons/ci";
+import { FaMoon } from "react-icons/fa";
+import * as S from "./style";
 
 interface Props {
   toggleTheme(): void;
@@ -15,24 +17,48 @@ export const Header: React.FC<Props> = ({ toggleTheme }) => {
 
   return (
     <HeaderContainer>
-
       <S.Link href="/">
         <img src={logoimg} alt="Logo Crypto" />
       </S.Link>
 
       <Switch
-            onChange={toggleTheme}
-            checked={title === "light"}
-            uncheckedIcon={false}
-            checkedIcon={false}
-            onColor="#30beff"
-            offColor="#444444"
-            offHandleColor="#30beff"
-            onHandleColor="#FFF"
-            handleDiameter={15}
-            width={40}
-            height={20}
-          />
+        onChange={toggleTheme}
+        checked={title === "light"}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        onColor="#ffffff"
+        offColor="#444444"
+        offHandleColor="#000000"
+        onHandleColor="#d9ff00"
+        handleDiameter={25}
+        width={50}
+        height={20}
+        uncheckedHandleIcon={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              color: "#FFF",
+            }}
+          >
+            <FaMoon size={14} />
+          </div>
+        }
+        checkedHandleIcon={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
+            <CiSun size={25} />
+          </div>
+        }
+      />
     </HeaderContainer>
   );
 };
