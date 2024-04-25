834b602c84d187c8ea2ec3446e740aaaa1ba565e
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-25 16:01:11 -0300

    feat: Create Content component for centering content
    This commit adds a Content component to the project. The Content component is responsible for centering and containing the main content of the application, ensuring a visually appealing and balanced layout.

---
+++ cryptocurrency-project/src/components/content/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './style';
+
+export const Content = ({children}: {children: React.ReactNode}) => {
+    return(
+        <S.Content>
+            {children}
+        </S.Content>
+    )
+}
