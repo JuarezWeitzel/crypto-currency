8dafdc204732f89be609224b17e13fe723cc9b3c
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-25 15:47:13 -0300

    refactor: Restructure home page
    This commit refactors the home page, restructuring its components and layout for improved organization and clarity. The changes aim to enhance the overall structure and readability of the home page.

--- cryptocurrency-project/src/pages/home/index.tsx
+++ cryptocurrency-project/src/pages/home/index.tsx
@@ -1,12 +1,20 @@
-import { Search } from "../../components/search"
-import { TableCurrency } from "../../components/tableCurrency"
-
+import { Search } from "../../components/search";
+import { TableCurrency } from "../../components/tableCurrency";
+import { Content } from "../../components/content";
+import { Container } from "../../components/container";
+import { Footer } from "../../components/footer";
 
 export const Home = () => {
-    return(
-        <>
-            <Search/>
-            <TableCurrency/>
-        </>
-    )
-}
+  return (
+    <Container>
+
+      <Content>
+        <Search />
+        <TableCurrency />
+      </Content>
+
+      <Footer />
+       
+    </Container>
+  );
+};
