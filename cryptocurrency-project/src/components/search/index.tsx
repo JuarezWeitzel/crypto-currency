b4efbd7026a4d8e18c36b8ed11446348f5676ad7
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-25 16:05:53 -0300

    feat: Add theme colors to BiSearch component

--- cryptocurrency-project/src/components/search/index.tsx
+++ cryptocurrency-project/src/components/search/index.tsx
@@ -1,38 +1,37 @@
-import { FormEvent, useState } from "react"
-import { MainContainer, SearchForm, SearchInput, SearchButton } from "./style"
-import { BiSearch } from "react-icons/bi"
+import { FormEvent, useContext, useState } from "react";
+import { MainContainer, SearchForm, SearchInput, SearchButton } from "./style";
+import { BiSearch } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
+import { ThemeContext } from "styled-components";
 
 export const Search = () => {
+  const themeContext = useContext(ThemeContext);
+  const colors = themeContext?.colors;
 
-    const [inputValue, setInputValue] = useState("");
-    const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
 
-    const handleSearch = (event: FormEvent) => {
-        event.preventDefault();
+  const handleSearch = (event: FormEvent) => {
+    event.preventDefault();
 
-        if(inputValue === "") return;
+    if (inputValue === "") return;
 
-        navigate(`/detail/${inputValue}`)
-    }
+    navigate(`/detail/${inputValue}`);
+  };
 
-    return(
-        <MainContainer>
-            <SearchForm onSubmit={handleSearch}>
-                <SearchInput 
-                value={inputValue}
-                onChange={(event) => setInputValue(event.target.value)}
-                placeholder="Enter currency symbol: Example BTC"
-                type="text"/>
-                <SearchButton
-                type="submit"
-                >
-                    <BiSearch
-                    size={30}
-                    color="#fff"
-                    />
-                </SearchButton>
-            </SearchForm>
-        </MainContainer>
-    )
-}
+  return (
+    <MainContainer>
+      <SearchForm onSubmit={handleSearch}>
+        <SearchInput
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          placeholder="Enter currency symbol: Example BTC"
+          type="text"
+        />
+        <SearchButton type="submit">
+          <BiSearch size={30} color={colors?.text} />
+        </SearchButton>
+      </SearchForm>
+    </MainContainer>
+  );
+};
