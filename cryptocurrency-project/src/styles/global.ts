ba833372afa33fb84238d11718e56aa319fdf37c
JuarezWeitzel <juarez.zbw@gmail.com>
2024-04-25 15:41:06 -0300

    feat: Add scrollbar and adjust body display
    This commit adds a scrollbar to the application and adjusts the display properties of the body element. The scrollbar enhances the usability of the application, while the display adjustments ensure proper layout rendering.

--- cryptocurrency-project/src/styles/global.ts
+++ cryptocurrency-project/src/styles/global.ts
@@ -5,12 +5,28 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        ::-webkit-scrollbar {
+        width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: ${(props) => props.theme.colors.text};
+}
+
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
     }
 
     body {
-        background-color: ${props => props.theme.colors.background};
+        display: flex;
+        flex-direction: column;
+        background-color: ${(props) => props.theme.colors.background};
         width: 100%;
-        max-width: 1080px;
         margin: 0 auto;
         font-family: sans-serif;
         font-size: 16px;
