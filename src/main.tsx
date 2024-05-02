c8e4360c5d204bf3bbfb7d7d32e8cd0d9e1cde2d
JuarezWeitzel <juarez.zbw@gmail.com>
2024-05-02 19:18:56 -0300

    feat: Import i18n into main file

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { StyleSheetManager } from 'styled-components';
+import './i18n/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
